928a4b74c512941a6ae0d5d5ae1d73259448b68a
Kaouthar Elidrissi Abderrahim <kaoutharidrissilink@gmail.com>
2021-08-29 14:28:09 +0200

    first commit

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../user';
+import { UserServiceService } from '../user-service.service';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss']
+})
+export class UserFormComponent  {
+
+  user!: User;
+
+  constructor(  private route: ActivatedRoute, 
+                private router: Router, 
+                private userService: UserServiceService) {
+    
+    
+      this.user = new User();
+  
+   }
+
+  
+
+  onSubmit() {
+    this.userService.save(this.user).subscribe(result => this.gotoUserList());
+  }
+
+  gotoUserList() {
+    this.router.navigate(['/users']);
+  }
+
+}
